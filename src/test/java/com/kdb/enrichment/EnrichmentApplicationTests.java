54763cc2becd4138add88f8f43d1079ce7e479ba
Bruno Limas <bruno.limas@kardbank.com.br>
2022-12-22 13:51:08 -0300

    initial commit

---
+++ src/test/java/com/kdb/enrichment/EnrichmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdb.enrichment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrichmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
